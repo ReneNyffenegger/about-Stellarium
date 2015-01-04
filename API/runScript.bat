1b0ca9aeb5207c4f966734bbaa3b79f72d666b61
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-01-04 19:47:17 +0100

    Suppress message that file was copied

--- API/runScript.bat
+++ API/runScript.bat
@@ -1,4 +1,4 @@
-@copy /y %1 "%programfiles%\Stellarium\scripts" 
+@copy /y %1 "%programfiles%\Stellarium\scripts"  > nul
 
 @pushd "%programfiles%\Stellarium"
 @stellarium --startup-script=%1 --full-screen=no
